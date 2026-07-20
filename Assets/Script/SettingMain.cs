31e8b6024713a1d45678772053d5d9c43db50130
cxkdev <rattanan24631@gmail.com>
2026-07-20 20:50:50 +0700

    20/07

--- Assets/Script/SettingMain.cs
+++ Assets/Script/SettingMain.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainSetting : MonoBehaviour
+public class SettingMain : MonoBehaviour
 {
     public GameObject settingSubPage;   
     public GameObject commandSubPage; 
